07be5d62be44dfeffd4ebe41f9cfdedcfc86b156
AfterWin <CJ_Zheng1023@hotmail.com>
2018-04-19 11:07:55 +0800

    init

--- phantom0100.js
+++ phantom0100.js
@@ -1,5 +1,4 @@
 let phantom = require('phantom')
-let sitePage = null
 let pInstance = null
 let p = null
 var t = Date.now();
@@ -11,29 +10,15 @@ phantom.create().then((instance) => {
     return page.open('http://www.baidu.com')
 }).then((status) => {
     if(status === "success") {
-        console.log('start render')
-        p.render('example.png');
-        p.evaluate(function(){
-            console.log(document.title)
-            //t = Date.now() - t
-            //console.log(t)
-            //pInstance.exit()
+        return p.invokeMethod('evaluate', function(){
+            return document.querySelector('body').innerHTML
         })
-        /*p.includeJs('https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js', function(){
-            p.evaluate(function(){
-                console.log(document.title)
-                //let html = $('body').html()
-                //console.log(html)
-                t = Date.now() - t
-                console.log(t)
-                pInstance.exit()
-            })
-        })*/
-    }else{
-        console.log('error')
-        throw new Error('url is error')
-        pInstance.exit()
     }
+}).then(function(html){
+    console.log(html)
+    t = Date.now() - t
+    console.log(t)
+    pInstance.exit()
 }).catch((e) => {
     console.log(e)
     pInstance.exit()
