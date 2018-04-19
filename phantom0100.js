b18ac2e0602807ce11f227bd48a154cf7842e99a
AfterWin <CJ_Zheng1023@hotmail.com>
2018-04-19 11:13:49 +0800

    init

--- phantom0100.js
+++ phantom0100.js
@@ -10,10 +10,13 @@ phantom.create().then((instance) => {
     return page.open('http://www.baidu.com')
 }).then((status) => {
     if(status === "success") {
-        return p.invokeMethod('evaluate', function(){
-            return document.querySelector('body').innerHTML
-        })
+        return p.invokeAsyncMethod('includeJs', 'https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js')
     }
+}).then(function(){
+    return p.invokeMethod('evaluate', function(){
+        return $('body').html()
+        //return document.querySelector('body').innerHTML
+    })
 }).then(function(html){
     console.log(html)
     t = Date.now() - t
